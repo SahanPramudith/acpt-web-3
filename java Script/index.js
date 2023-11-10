eac3cf70fbadabebf5ababb92ab9b8453c03be23
sahanpramudith <pramuditsahan466@gmail.com>
2023-11-10 22:13:01 +0530

    day-14

--- java Script/index.js
+++ java Script/index.js
@@ -2,9 +2,67 @@ console.log('Matara');
 document.write("sahan");
 document.write('<h1>bogahawatta');
 
-alert('sahan');
+// alert('sahan');
 
 var name="sahan";
 {
     var name="nnn";
 }
+var a="run"
+var a="bb"
+
+let c =12;
+console.log(c);
+
+c = 15;
+// console.log(c);
+// {
+//    let d = 60;
+// }
+// console.log(d);
+
+// const con =12;
+// const con  =12;
+
+const con = "5";
+console.log(con);
+
+// {
+//     const p = "40";
+
+// }
+// console.log(p);
+
+const w = 12;
+const q = 10;
+
+const addision =w+q;
+console.log(addision);
+
+const subraction = w-q;
+console.log(subraction);
+
+let h =100;
+h++
+console.log(h);
+
+var r = 500;
+r--
+console.log(r);
+
+const z =10;
+const x=11;
+
+console.log(z==x);
+console.log(z!=x);
+console.log(z>x);
+console.log(z<x);
+console.log(z>=x);
+console.log("less tha or equal =",z<=x);
+
+const p = true;
+const e = false;
+
+console.log("and",p&&e);
+console.log("or",p||e);
+console.log("not",!e);
