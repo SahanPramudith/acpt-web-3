c02bcbe185007649fe9ed9f82078e0be9188b60d
sahanpramudith <pramuditsahan466@gmail.com>
2023-11-21 15:32:04 +0530

    new

--- java Script/index.js
+++ java Script/index.js
@@ -8,19 +8,37 @@ var name="sahan";
 {
     var name="nnn";
 }
+    // var
+
 var a="run"
 var a="bb"
+console.log(a);
+
+{
+    var a = "55"
+}
+console.log(a);
 
 let c =12;
 console.log(c);
 
 c = 15;
-// console.log(c);
+ console.log(c);
+
+//  let
+
+let na = 500
+console.log(na);
+
 // {
 //    let d = 60;
 // }
 // console.log(d);
 
+// const
+
+na=1000
+console.log(na);
 // const con =12;
 // const con  =12;
 
@@ -66,3 +84,99 @@ const e = false;
 console.log("and",p&&e);
 console.log("or",p||e);
 console.log("not",!e);
+
+var a = "sahan"
+var a = "pramudith"
+
+// function
+
+function myage(){
+    console.log("my age : 20");
+}
+myage()
+var mycity = ()=>{
+    console.log("city : matara");
+}
+mycity()
+
+
+function myName (name,addres) {
+    console.log("my mane is : ", name);
+    console.log("my addres is : ",addres);
+}
+myName("sahan","matara");
+
+
+
+let name2 = (name) => {
+    console.log("my 2 name :",name);
+}
+name2("bogahawatta");
+
+ function myhome (home,name) {
+    console.log("may home : ",home);
+    console.log("may name is: ",name);
+ }
+ myhome("ganadara","sahan")
+
+ function numbere (nuber1,nuber2){
+    const addision = nuber1+nuber2;
+    console.log("mubere =",addision);
+ }
+ numbere(100,200)
+
+ function multipication  (nuber3,nuber4){
+    var muti = nuber3*nuber4
+    // console.log("mulitipication : ",muti);
+    return muti
+
+ }
+ var my = multipication(5,5)
+ console.log(my);
+// multipication(4,2)
+// multipication(5,2)
+// multipication(5,7)
+
+function subrac (content1,content2) {
+    var sub = content1+content2
+    // console.log(addi);
+    return sub
+    
+}
+var same = subrac (1000,200)
+console.log(same);
+
+
+var funtion = (num1, num2, num3, num4,)=>{
+    var total = num1+num2+num3+num4
+    var avg = total/4
+    // console.log("avvg = ",avg);
+    return avg
+}
+let re = funtion(50,20,30,40)
+console.log(re);
+// funtion(10,20,30,40)
+
+// flow controlls
+
+// if
+const val =60
+
+if (50<val) {
+    console.log("pass");
+    
+}else{
+    console.log("fail");
+}
+
+var marks =80
+
+if (marks>50) {
+    console.log("A");
+} else if (marks>65) {
+    console.log("B");
+}else {
+    console.log("c");
+}
+
+
